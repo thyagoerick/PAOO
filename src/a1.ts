0910e663ec9722d65dc6e91f8c40ce223b10509a
aluno <thyago123gois@gmail.com>
2022-02-19 08:36:00 -0300

    .

--- src/a1.ts
+++ src/a1.ts
@@ -1,5 +1,3 @@
-
-                                   
 /*
                                      //Natureza do valor devolvido, pode ser omitido
 function sum(a: number, b: number): number{
@@ -10,10 +8,28 @@ function sum(a: number, b: number){
     return a + b;
 }
 */
-
-
+/*
 function sum(a: bigint, b: bigint): bigint{
     return a + b;
 }
 //Para informar que o valor é bigint colocamos "n"  no final do valor (JS)
-console.log(sum(10n,20n));
+//console.log(sum(10n,20n));
+
+function fat(n: bigint) {
+    let f = 1n;
+    for(let i=2n; i <=n ; ++i)
+        f *=i; 
+    return f;
+}
+console.log(fat(50n));
+*/
+
+function rfat(n: bigint): bigint {
+    return n == 0n 
+            ? 1n 
+            : n * rfat(n-1n);
+}
+
+console.log(rfat(3n)); 
+
+
