b5e428304470d554df599cd6c44707d144e40352
aluno <thyago123gois@gmail.com>
2022-02-19 06:50:59 -0300

    Aula 01 de Programaçao Avançada Orientada a Objetos

---
+++ aula02_2022-02-19/a1.js
@@ -0,0 +1,10 @@
+//Natureza do valor devolvido, pode ser omitido
+function sum(a, b) {
+    return a + b;
+}
+/*
+function sum(a: number, b: number){
+    return a + b;
+}
+*/
+console.log(sum(10, 20));
---
+++ aula02_2022-02-19/a1.ts
@@ -0,0 +1,12 @@
+
+                                    //Natureza do valor devolvido, pode ser omitido
+function sum(a: number, b: number): number{
+    return a + b;
+}
+/*
+function sum(a: number, b: number){
+    return a + b;
+}
+*/
+
+console.log(sum(10,20))
