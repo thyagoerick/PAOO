b0f11e86e0e5d975e3ece5ba5fcde2e71ee9cd79
aluno <thyago123gois@gmail.com>
2022-02-19 08:06:22 -0300

    Configurando diretorio da materia

---
+++ dist/a1.js
@@ -0,0 +1,16 @@
+"use strict";
+/*
+                                     //Natureza do valor devolvido, pode ser omitido
+function sum(a: number, b: number): number{
+    return a + b;
+}
+
+function sum(a: number, b: number){
+    return a + b;
+}
+*/
+function sum(a, b) {
+    return a + b;
+}
+//Para informar que o valor é bigint colocamos "n"  no final do valor (JS)
+console.log(sum(10n, 20n));
