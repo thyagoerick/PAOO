bf14caa2a92812fc55f69d1f6638695e1317ce22
aluno <thyago123gois@gmail.com>
2022-02-19 08:34:20 -0300

    .

--- dist/a1.js
+++ dist/a1.js
@@ -9,8 +9,24 @@ function sum(a: number, b: number){
     return a + b;
 }
 */
-function sum(a, b) {
+/*
+function sum(a: bigint, b: bigint): bigint{
     return a + b;
 }
 //Para informar que o valor é bigint colocamos "n"  no final do valor (JS)
-console.log(sum(10n, 20n));
+//console.log(sum(10n,20n));
+
+function fat(n: bigint) {
+    let f = 1n;
+    for(let i=2n; i <=n ; ++i)
+        f *=i;
+    return f;
+}
+console.log(fat(50n));
+*/
+function rfat(n) {
+    return n == 0n
+        ? 1n
+        : n * rfat(n - 1n);
+}
+console.log(rfat(3n));
