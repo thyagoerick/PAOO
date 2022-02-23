d09139cd9c7f17fcfbf3e0241fdec6fd35d66abb
Thyago Erick <thyago123gois@gmail.com>
2022-02-23 12:54:06 -0300

    Atualizando arquivo .ts

--- dist/a1.js
+++ dist/a1.js
@@ -1,32 +1,33 @@
 "use strict";
-/*
-                                     //Natureza do valor devolvido, pode ser omitido
-function sum(a: number, b: number): number{
+//Natureza do valor devolvido, pode ser omitido
+function sumNumber(a, b) {
     return a + b;
 }
-
+/*
 function sum(a: number, b: number){
     return a + b;
 }
 */
-/*
-function sum(a: bigint, b: bigint): bigint{
+console.log("Soma (Number): " + sumNumber(55, 5));
+/*****************************************************************/
+function sumBigint(a, b) {
     return a + b;
 }
 //Para informar que o valor é bigint colocamos "n"  no final do valor (JS)
-//console.log(sum(10n,20n));
-
-function fat(n: bigint) {
+console.log("\nSoma (Bigint): " + sumBigint(10n, 20n));
+/*****************************************************************/
+function fat(n) {
     let f = 1n;
-    for(let i=2n; i <=n ; ++i)
-        f *=i;
+    for (let i = 2n; i <= n; ++i)
+        f *= i;
     return f;
 }
-console.log(fat(50n));
-*/
+console.log("\nFatorial: " + fat(5n));
+/*****************************************************************/
+// Operador Ternário:  condição ? casoTrue : casoFalse
 function rfat(n) {
     return n == 0n
         ? 1n
         : n * rfat(n - 1n);
 }
-console.log(rfat(3n));
+console.log("\nFatorial Recursivo: " + rfat(3n));
--- src/a1.ts
+++ src/a1.ts
@@ -1,19 +1,24 @@
-/*
+
                                      //Natureza do valor devolvido, pode ser omitido
-function sum(a: number, b: number): number{
+function sumNumber(a: number, b: number): number{
     return a + b;
 }
-
+/*
 function sum(a: number, b: number){
     return a + b;
 }
 */
-/*
-function sum(a: bigint, b: bigint): bigint{
+console.log("Soma (Number): " + sumNumber(55, 5));
+
+/*****************************************************************/
+
+function sumBigint(a: bigint, b: bigint): bigint{
     return a + b;
 }
 //Para informar que o valor é bigint colocamos "n"  no final do valor (JS)
-//console.log(sum(10n,20n));
+console.log("\nSoma (Bigint): " + sumBigint(10n,20n)); 
+
+/*****************************************************************/
 
 function fat(n: bigint) {
     let f = 1n;
@@ -21,15 +26,16 @@ function fat(n: bigint) {
         f *=i; 
     return f;
 }
-console.log(fat(50n));
-*/
+console.log("\nFatorial: " + fat(5n));
 
+/*****************************************************************/
+
+// Operador Ternário:  condição ? casoTrue : casoFalse
 function rfat(n: bigint): bigint {
     return n == 0n 
             ? 1n 
             : n * rfat(n-1n);
 }
-
-console.log(rfat(3n)); 
+console.log("\nFatorial Recursivo: " + rfat(3n)); 
 
 
