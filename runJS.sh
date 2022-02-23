9fdd239451189c606fd185728a67b6582225fef1
Thyago Erick <thyago123gois@gmail.com>
2022-02-23 12:55:32 -0300

    Criando arquivo para rodar .js sem ter que entrar manualmente na pasta dist

---
+++ runJS.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Author: Thyago Erick Silva Gois (@7.3.5.6)
+cd dist/;
+echo -ne "\n________________|RESULT|________________\n\n"
+    node $1.js
+echo -ne "\n________________________runJS_By @7.3.5.6\n\n"
+exit 0
